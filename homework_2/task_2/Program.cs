f485db9bbf0e82d3fe985907c0355255baabc26a
Aleksey Mikheev <aleksvalmix@yandex.ru>
2023-06-26 13:17:27 +0500

    homework 2

---
+++ homework_2/task_2/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 2: Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+// что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+int ReadInt(string message)
+{
+    System.Console.WriteLine(message);
+    string inputedStr = Console.ReadLine();
+    int number = Convert.ToInt32(inputedStr);
+    return number;
+}
+
+int number = ReadInt("Введите трехзначное число число");
+if (number < 100)
+{
+    System.Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    string strNumber = Convert.ToString(number);
+
+    System.Console.WriteLine(strNumber[2]);
+}
+
---
+++ homework_2/task_3/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 3: Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+
+
+int ReadInt(string message)
+{
+    System.Console.WriteLine(message);
+    string inputedStr = Console.ReadLine();
+    int number = Convert.ToInt32(inputedStr);
+    return number;
+}
+
+int number = ReadInt("Введите цифру день недели");
+
+if (number < 1 || number > 7)
+{
+    System.Console.WriteLine("Некорректный ввод");
+}
+else if (number >= 6)
+{
+    System.Console.WriteLine("да");
+}
+else
+{
+    System.Console.WriteLine("нет");
+}
+
