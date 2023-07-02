f47e4984b00e2ab7cffde14df8fc32ebf7a67233
Aleksey Mikheev <aleksvalmix@yandex.ru>
2023-07-02 16:14:25 +0500

    hw3

---
+++ homewwork_3/task_1/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 1
+//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом. Не использовать строки (только / и % используем)
+//14212 -> нет
+//12821 -> да
+//23432 -> да
+
+
+int ReadInt(string message)
+{
+    System.Console.WriteLine(message);
+    string inputedStr = Console.ReadLine();
+    int number = Convert.ToInt32(inputedStr);
+    return number;
+}
+
+int number = ReadInt("Введите пятизначное число число");
+
+int fives = number % 10;
+
+int fourth = number / 10 % 10;
+
+int third = number / 10 / 10 % 10;
+
+int second = number / 10 / 10 / 10 % 10;
+
+int first = number / 10 / 10 / 10 / 10 % 10;
+
+
+
+if (fives == first && fourth == second)
+{
+    System.Console.WriteLine("Введенное число является полиндромом");
+}
+else
+{
+    System.Console.WriteLine("Введенное число не является полиндромом");
+}
+
+System.Console.WriteLine($"{first} {second} {third} {fourth} {fives}");
---
+++ homewwork_3/task_3/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 3
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("Hello, World!");
+int ReadInt(string message)
+{
+    System.Console.WriteLine(message);
+    string inputedStr = Console.ReadLine();
+    int number = Convert.ToInt32(inputedStr);
+    return number;
+}
+int number = ReadInt("Введите число");
+
+for (int i = 1; i <= number; i++)
+{
+    if (i == number)
+    {
+        System.Console.Write(i * i * i);
+    }
+    else
+    {
+        System.Console.Write(i * i * i + ", ");
+    }
+
+}
