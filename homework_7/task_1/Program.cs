785bac1c45c9ef6ae302dc9f100c07c74d9d7db3
Aleksey Mikheev <aleksvalmix@yandex.ru>
2023-07-18 13:18:00 +0500

    hw7

---
+++ homework_7/task_1/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 1. Задайте двумерный массив размером m×n, 
+// заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+int ReadInt(string message)
+{
+    System.Console.WriteLine(message);
+    string inputedStr = Console.ReadLine();
+    int number = Convert.ToInt32(inputedStr);
+    return number;
+}
+
+double[,] GenerateArray(int rows, int cols)
+{
+    double[,] array = new double[rows, cols];
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.NextDouble() * 10.0 - 5;
+        }
+    }
+
+    return array;
+}
+
+void printD2Array(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]}\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+double[,] array = GenerateArray(ReadInt("Введите количество строк >"), ReadInt("Введите количество колонок >"));
+printD2Array(array);
---
+++ homework_7/task_3/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задача 3. Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int ReadInt(string message)
+{
+    System.Console.WriteLine(message);
+    string inputedStr = Console.ReadLine();
+    int number = Convert.ToInt32(inputedStr);
+    return number;
+}
+
+int[,] GenerateArray()
+{
+    int[,] array = new int[3, 3];
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(0, 10);
+        }
+    }
+
+    return array;
+}
+
+void printD2Array(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]}\t");
+        }
+        System.Console.WriteLine();
+    }
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        double result = new double();
+        double preResult = (array[0, i] + array[1, i] + array[2, i]);
+        result = Math.Round((preResult / 3), 2);
+        System.Console.Write($"{result}\t");
+
+    }
+}
+
+int[,] array = GenerateArray();
+printD2Array(array);
